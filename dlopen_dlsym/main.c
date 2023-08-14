7857d08af48c69d0f25f2c98f1aba8b3ad9e5a1c
asientador <valiente.pablo@proton.me>
2023-08-14 09:33:40 +0200

    dlopen_dlsym

---
+++ dlopen_dlsym/main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <dlfcn.h>
+
+int main() {
+
+    void *handle = dlopen("./liblibrary.so", RTLD_LAZY);
+
+    if (!handle){
+        printf("dlopen returned null handle\n");
+        printf("Error description: %s\n",dlerror());
+    return -1;
+    }
+
+    void (*functionPointer)() = dlsym(handle,"print_hello");
+
+    if(!functionPointer){
+        printf("dlsym returned null pointer\n");
+        printf("Error description %s\n",dlerror());
+        return -1;
+    }
+
+    functionPointer();
+
+    dlclose(handle);
+    return 0;
+}
+
